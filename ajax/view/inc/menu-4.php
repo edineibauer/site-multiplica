0af3d2e504e1a6f2fab800fac60e44108d12d1ae
Edinei <galera.org@gmail.com>
2018-06-21 19:31:46 -0300

    ups

--- ajax/view/inc/menu-4.php
+++ ajax/view/inc/menu-4.php
@@ -1,34 +1,38 @@
 <?php
 $read = new \ConnCrud\Read();
 $read->exeRead(PRE . "credenciado_juridico", "WHERE usuario = :ui", "ui={$_SESSION['userlogin']['id']}");
-if(!$read->getResult())
+if (!$read->getResult())
     $read->exeRead(PRE . "credenciado_fisico", "WHERE usuario = :ui", "ui={$_SESSION['userlogin']['id']}");
-if(!$read->getResult())
+if (!$read->getResult())
     $read->exeRead(PRE . "funcionario_credenciado", "WHERE usuario = :ui", "ui={$_SESSION['userlogin']['id']}");
 
-if($read->getResult()) {
+if ($read->getResult()) {
     $cred = $read->getResult()[0];
     ?>
     <div class="col padding-medium">
-        <div class="col menu-li align-center border padding-medium margin-bottom color-grey-light opacity radius pointer hover-opacity-off"
+        <div class="col menu-li align-center hover-text-theme border padding-medium color-grey-light opacity radius pointer hover-opacity-off"
+             style="margin-bottom: 5px"
              data-action="page" data-atributo="dash/geral">
             <i class="font-xxxlarge material-icons">timeline</i>
             <span class="font-large col">Dashboard</span>
         </div>
-        <div class="col align-center border padding-medium margin-bottom color-grey-light opacity radius pointer hover-opacity-off"
+        <div class="col align-center border hover-text-theme padding-medium color-grey-light opacity radius pointer hover-opacity-off"
+             style="margin-bottom: 5px"
              onclick="createTransacao()">
             <i class="font-xxxlarge material-icons">add_box</i>
             <span class="font-large col">Pontuar Multiplica</span>
         </div>
-        <div class="col align-center border padding-medium margin-bottom color-grey-light opacity radius pointer hover-opacity-off"
+        <div class="col align-center border hover-text-theme padding-medium color-grey-light opacity radius pointer hover-opacity-off"
+             style="margin-bottom: 5px"
              onclick="createCliente()">
             <i class="font-xxxlarge material-icons">people</i>
-            <span class="font-large col">Novo Conveniado</span>
+            <span class="font-large col">Novo Cliente</span>
         </div>
         <?php
         if ($_SESSION['userlogin']['nivel'] === "1") {
             ?>
-            <div class="col menu-li align-center border padding-medium margin-bottom color-grey-light opacity radius pointer hover-opacity-off"
+            <div class="col menu-li align-center hover-text-theme border padding-medium color-grey-light opacity radius pointer hover-opacity-off"
+                 style="margin-bottom: 5px"
                  data-entity='funcionario_credenciado' data-action="table">
                 <i class="font-xxxlarge material-icons">people_outline</i>
                 <span class="font-large col">Funcionários</span>
@@ -37,7 +41,8 @@ if($read->getResult()) {
         }
         if (isset($cred['desconto_em_servicos']) && $cred['desconto_em_servicos']) {
             ?>
-            <div class="col menu-li align-center border padding-medium margin-bottom color-grey-light opacity radius pointer hover-opacity-off"
+            <div class="col menu-li align-center hover-text-theme border padding-medium color-grey-light opacity radius pointer hover-opacity-off"
+                 style="margin-bottom: 5px"
                  data-entity='descontos' data-action="table">
                 <i class="font-xxxlarge material-icons">book</i>
                 <span class="font-large col">Tabela de Descontos</span>
@@ -53,26 +58,43 @@ if($read->getResult()) {
     </div>
 
     <script>
+        var updateHist = null;
+        var $dash = $("#dashboard");
 
         function mainLoading() {
             $(".main").loading();
             hide_sidebar_small();
             closeSidebar();
+            clearInterval(updateHist);
         }
 
         function createCliente() {
             mainLoading();
             get('convenio/create/cliente', function (data) {
-                $("#dashboard").html(data.content)
+                $dash.html(data.content)
             })
+        }
+
+        function updateHistorico(dd, tt) {
+            if(typeof (tt) === "undefined")
+                toast("Pontos Aplicados");
+
+            var $ref = $dash.find("#credenciado-historico-ref").html("");
+            var $hist = $dash.find("#credenciado-historico");
+            get('credenciado/read/historico', function (data) {
+                $ref.template(data.content.template, data.content, function(data) {
+                    if (data !== "" && data !== $hist.html())
+                        $hist.find("ul:eq(0)").replaceWith(data);
+                });
+            });
         }
 
         function createTransacao() {
             mainLoading();
             get('credenciado/create/transacao', function (data) {
-                console.log(data);
-                $("#dashboard").html(data.content)
-            }, true)
+                $dash.html(data.content);
+                updateHistorico(1, 1);
+            });
         }
 
         /*function createPedido() {
