bb9a020f4ec0f12910e1cd8d51c123267d9d6e7b
Edinei <galera.org@gmail.com>
2018-11-29 14:59:46 -0200

    credenciados

--- public/react/usuarios/update.php
+++ public/react/usuarios/update.php
@@ -1,19 +1,31 @@
 <?php
-
 /* Reenvia SMS caso seja alterado */
-if(!empty($dados['telefone']) && $dadosOld['telefone'] !== $dados['telefone']) {
+if (!empty($dados['telefone']) && $dadosOld['telefone'] !== $dados['telefone']) {
     try {
         //Prepara para enviar email
-        $telDic = new \Entity\Dicionario("smsiagente");
-        $telDic->setData([
-            "celular" => $dados['telefone'],
-            "mensagem" => "Bem vindo(a) a MULTIPLICA, utilize o APP para android http://bit.ly/2qhm6la, acesse com seu Email ou CPF/CNPJ, juntamente com a senha: {$senha}",
-            "status" => "AGUARDANDO"
-        ]);
-        $telDic->save();
 
-        if ($telDic->getError())
-            $data['error'] = $telDic->getError();
+        $read = new \ConnCrud\Read();
+        $read->exeRead("clientes", "WHERE login = :ll", "ll={$dados['id']}");
+        if ($read->getResult()) {
+            $cli = $read->getResult()[0];
+            $senha = (!empty($cli['cpf']) ? substr($cli['cpf'], 0, 4) : (!empty($cli['email']) ? explode("@", $cli['email'])[0] : substr($dados['telefone'], -4)));
+
+            //atualiza senha do usuário para garantir que a senha enviada por SMS vai ser válida
+            $up = new \ConnCrud\Update();
+            $up->exeUpdate("usuarios", ['password' => \Helpers\Check::password($senha)], "WHERE id = :id", "id={$dados['id']}");
+
+            //envia acesso por SMS
+            $telDic = new \Entity\Dicionario("smsiagente");
+            $telDic->setData([
+                "celular" => $dados['telefone'],
+                "mensagem" => "Bem vindo(a) a MULTIPLICA, utilize o APP para android http://bit.ly/2qhm6la, acesse com seu Email ou CPF/CNPJ, juntamente com a senha: {$senha}",
+                "status" => "AGUARDANDO"
+            ]);
+            $telDic->save();
+
+            if ($telDic->getError())
+                $data['error'] = $telDic->getError();
+        }
 
     } catch (Exception $exception) {
         $data['error'] = "Erro de Exception ao Enviar Email";
